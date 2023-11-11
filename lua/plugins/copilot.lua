6fe7907ebe88c39863bbc57febd56eec61b10ec3
Christopher Lam <chrislam67@gmail.com>
2023-11-11 17:06:21 -0600

    Update copilot.lua remap keys for accept, next, etc

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,6 +1,9 @@
 -- this is for a bug in copilot.vim, suggestions won't work w/o this
 -- https://www.reddit.com/r/neovim/comments/wbx4r6/has_anyone_managed_to_use_github_copilot_in_nvchad/
 vim.g.copilot_assume_mapped = true
+vim.keymap.set("i", "<C-i>", "copilot#Complete()", {expr=true, silent=true})
+vim.keymap.set("i", "<C-j>", "copilot#Next()", {expr=true, silent=true})
+vim.keymap.set("i", "<C-k>", "copilot#Previous()", {expr=true, silent=true})
 
 return {
   {
